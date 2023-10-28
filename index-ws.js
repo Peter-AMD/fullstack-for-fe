f3ab38c978016015892b3edb00e853203d72d12b
Peter Anthony Melecio Duot <gpeteamd@gmail.com>
2023-10-28 18:59:05 +0800

    updat ws

--- index-ws.js
+++ index-ws.js
@@ -2,21 +2,21 @@ const express = require("express");
 const server = require("http").createServer();
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", function getRoot(req, res) {
   res.sendFile("index.html", { root: __dirname });
 });
 
 server.on("request", app);
-server.listen(3000, () => {
+server.listen(3000, function serverListen() {
   console.log("Server started on port 3000");
 });
 
 // Begin websocket
 const WebSocketServer = require("ws").Server;
 
-const wss = new WebSocketServer({ server });
+const wss = new WebSocketServer({ server: server });
 
-wss.on("connection", (ws) => {
+wss.on("connection", function wsConnection(ws) {
   const numClients = wss.clients.size;
   console.log("Clients connected", numClients);
 
@@ -26,13 +26,13 @@ wss.on("connection", (ws) => {
     ws.send("Welcome to my server");
   }
 
-  ws.on("close", () => {
+  ws.on("close", function wsClose() {
     console.log("A client has disconnected");
     wss.broadcast(`Current visitors: ${numClients}`);
   });
 });
 
-wss.broadcast = (data) => {
+wss.broadcast = function broadcast(data) {
   wss.clients.forEach((client) => {
     client.send(data);
   });
